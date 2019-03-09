89ddd7cb5ab55fa7454158867be17318e5aa6cb8
Joshua Kennedy <joshuakennedy102@gmail.com>
2019-03-09 18:48:18 +0200

    Clean up of capture verification and documentation

--- stream_downloader.py
+++ stream_downloader.py
@@ -3,29 +3,31 @@ import numpy as np
 
 def download_stream(url, outpath, num_frames):
     
-    cam = cv2.VideoCapture(url)
+    #create the webcam capture
+    capture = cv2.VideoCapture(url)
     
-    good_stream = False
+    if not capture.isOpened():
+        print("Unable to read stream")
+        return
+    
+    ret_initial, image_initial = capture.read()
 
-    while not good_stream:
-        
-        ret_1, image_1 = cam.read()
-        if image_1 is not None:
-            good_stream = True
-            input_shape = image_1.shape
-        else:
-            print("Unable to read stream")
-            cap.release()
-            return
+    #get the shape of the numpy array for creating the video writer
+    input_shape = image_initial.shape
 
-    print(input_shape)
+    print("Video Size: " + str(input_shape))
 
-    capture = video = cv2.VideoWriter(outpath, cv2.VideoWriter_fourcc('M','J','P','G'), 10, (input_shape[1], input_shape[0]))
+    #create video writer
+    video = cv2.VideoWriter(outpath, cv2.VideoWriter_fourcc('M','J','P','G'), 10, (input_shape[1], input_shape[0]))
 
-    i = 0
+    video.write(image_initial)
 
+    i = 1
+
+    #while loop since webcameras may run slower than the video can be encoded, will on increment on a successful
+    #read of the capture
     while i < num_frames:
-        ret, img = cam.read()
+        ret, img = capture.read()
             
         if img is not None:
             print(i)
@@ -36,8 +38,9 @@ def download_stream(url, outpath, num_frames):
             print("Dropped Frame")
 
 
+    #free up the captures and writers
     video.release()
-    cam.release()
+    capture.release()
     cv2.destroyAllWindows()
 
     return
